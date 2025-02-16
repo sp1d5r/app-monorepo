b0359105a43cf8228337dcfaa107cbf8f72c7a7e
Elijah <aa1719@imperial.ac.uk>
2025-02-16 16:00:49 +0000

    Frontend: Resolved react flow rpesent failure in babel config.

--- apps/expo-app/babel.config.js
+++ apps/expo-app/babel.config.js
@@ -6,42 +6,44 @@ module.exports = function (api) {
   console.log('Platform:', process.env.TAMAGUI_TARGET);
 
   return {
-    presets: ['babel-preset-expo'],
-    assumptions: {
-      'enumerableModuleMeta': true
-    },
+    presets: [
+      '@babel/preset-flow',
+      ['babel-preset-expo', {
+        jsxRuntime: 'automatic',
+      }],
+      ['module:@react-native/babel-preset']
+    ],
     plugins: [
+      ['@babel/plugin-transform-flow-strip-types', { 
+        all: true,
+        requireDirective: false,
+      }],
+      ['@babel/plugin-transform-react-jsx', {
+        flow: true,
+      }],
+      '@babel/plugin-proposal-export-namespace-from',
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       ['@babel/plugin-transform-runtime', {
-        corejs: 3,
+        corejs: false,
         helpers: true,
         regenerator: true,
-        version: '^7.26.9',
-        absoluteRuntime: false
+        version: '^7.22.5',
       }],
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@app-monorepo/ui': '../../packages/ui/src',
-            'react': '../../node_modules/react',
-            'react-dom': '../../node_modules/react-dom',
-            'react/jsx-runtime': '../../node_modules/react/jsx-runtime',
-            'react/jsx-dev-runtime': '../../node_modules/react/jsx-dev-runtime',
-          },
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          '@app-monorepo/ui': '../../packages/ui/src',
+          'react': '../../node_modules/react',
+          'react-dom': '../../node_modules/react-dom',
+          'react/jsx-runtime': '../../node_modules/react/jsx-runtime',
+          'react/jsx-dev-runtime': '../../node_modules/react/jsx-dev-runtime',
+          'react-native': '../../node_modules/react-native',
+          '@react-native/babel-preset': '../../node_modules/@react-native/babel-preset',
         },
-      ],
-      // [
-      //   '@tamagui/babel-plugin',
-      //   {
-      //     components: ['tamagui', '@app-monorepo/ui'],
-      //     config: './tamagui.config.ts',
-      //     importsWhitelist: ['constants.js', 'colors.js'],
-      //     logTimings: true,
-      //     disableExtraction: process.env.NODE_ENV === 'development',
-      //   },
-      // ],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json', '.d.ts'],
+      }],
       'transform-inline-environment-variables',
       'react-native-reanimated/plugin',
     ],
